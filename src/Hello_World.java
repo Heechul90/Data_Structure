5dbc6842585c6673fff4264dd5c252771b2f44e2
heechul <heechul4296@gmail.com>
2020-08-27 17:58:23 +0900

    study

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -1,5 +1,10 @@
+import sun.awt.AWTAccessor;
+
 public class Hello_World {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println();;
     }
 }
+
